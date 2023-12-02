d3f0916997b351ea6f3a1c4926048463fe75ec0c
AdamRzem <adamrzem09@gmail.com>
2023-12-02 06:59:43 +0100

    inne

--- src/routes/submitions/+page.svelte
+++ src/routes/submitions/+page.svelte
@@ -1,59 +1,104 @@
 <script lang="ts">
-    import type { PageData } from './$types';
-    import { AppBar, AppShell } from '@skeletonlabs/skeleton';
-    
-	import { initializeStores } from '@skeletonlabs/skeleton';
-    import { Modal, getModalStore } from '@skeletonlabs/skeleton';
-import type { ModalSettings, ModalComponent, ModalStore } from '@skeletonlabs/skeleton';
-initializeStores();
-const modalStore = getModalStore();
-    let visible: boolean = true;
+	import { db } from '$lib/db';
+	import { AppShell } from '@skeletonlabs/skeleton';
+	import { addDoc, collection } from 'firebase/firestore';
+    import { onMount } from 'svelte';
+    import { getDatabase, ref, onValue } from 'firebase/database';
 
-    const modal: ModalSettings = {
-	type: 'alert',
-	// Data
-	title: 'Wyslano dane',
-	body: 'Teraz masz szansze na zostanie wolontariuszem.',
-	image: 'ok.jpg',
-};
+
+	let im: string;
+	let nz: string;
+	let mi: string;
+	let ul: string;
+	let nr: string;
+
+
+    let data = [];
+
+    const database = getDatabase();
+    const usersRef = ref(database, 'vols');
+
+  onMount(() => {
+    onValue(usersRef, (snapshot) => {
+      const userData = snapshot.val();
+      if (userData) {
+        data = Object.values(userData);
+      }
+    });
+  });
+
 </script>
-<Modal/>
+
 <AppShell>
-    <div class="flex justify-center w-full h-full">
-    <form  action="/submitions">
-        <label class="label" for="imie">Imie</label>
-            <input class="input input-success" type="text" placeholder="Podaj Imie" id="imie"/>    
-        <label class="label" for="nazwisko">Nazwisko</label>            
-           <input class="input input-success" type="text" placeholder="Podaj Nazwisko" id="nazwisko"/>
-        <label class="label" for="adres">Zamieszkanie</label>            
-           <input class="input input-warning" type="text" placeholder="Podaj swoje miasto" id="adres"/>
-           <input class="input input-warning" type="text" placeholder="Podaj swoja ulice"/>
-           <input class="input input-warning" type="number" placeholder="Podaj numer domu"/>
-           <input class="input input-warning" type="text" placeholder="Podaj swoje wojewodctwo"/>
-           <input class="input input-warning" type="text" placeholder="Podaj swoj kraj"/>
-           <input class="input input-warning" type="text" placeholder="Podaj swoj kontynent"/>
-        <label class="label" for="dane">Dane osobowe</label>
-            <input class="input input-error" type="text" placeholder="Podaj imie i nazwisko matki" id="dane"/>
-            <input class="input input-error" type="text" placeholder="Podaj imie i nazwisko ojca"/>
-            <input class="input input-error" type="text" placeholder="Podaj swoje drugie imie(jak nie masz to nie wpisuj)"/><br><br>
-            <input type="submit" class="btn variant-filled rounded-full" value="Przeslij formulaz " id="kitty" on:click={()=>modalStore.trigger(modal)}>
-
-        </form>
+	<div class="flex justify-center w-full h-96">
+		<form
+			on:submit|preventDefault={async (e) => {
+				await addDoc(collection(db, 'vols'), { im, nz, mi, ul, nr });
+			}}
+		>
+			<label class="label" for="imie">Imie</label>
+			<input
+				class="input input-success"
+				type="text"
+				placeholder="Podaj Imie"
+				id="imie"
+				bind:value={im}
+			/>
+			<label class="label" for="nazwisko">Nazwisko</label>
+			<input
+				class="input input-success"
+				type="text"
+				placeholder="Podaj Nazwisko"
+				id="nazwisko"
+				bind:value={nz}
+			/>
+			<label class="label" for="adres">Zamieszkanie</label>
+			<input
+				class="input input-warning"
+				type="text"
+				placeholder="Podaj swoje miasto"
+				id="adres"
+				bind:value={mi}
+			/>
+			<input
+				class="input input-warning"
+				type="text"
+				placeholder="Podaj swoja ulice"
+				bind:value={ul}
+			/>
+			<input
+				class="input input-warning"
+				type="number"
+				placeholder="Podaj numer domu"
+				bind:value={nr}
+			/>
+			<input
+				type="submit"
+				class="btn variant-filled rounded-full"
+				value="Przeslij formulaz "
+				id="kitty"
+			/>
+		</form>
         
-
-        
-        
-       
+	</div>
+    <div class="flex justify-center w-full h-96">
+    <!-- <button class="btn text-black bg-slate-100 w-52 h-9 rounded-full" on:click={}>Pokaż innych wolontariuszy</button> -->
 </div>
+<ol class="flex">
+    {#each data as vols (vols.id)}
+      <li class="text-white">{vols.im} , {vols.mi}, {vols.nr}, {vols.nz}, {vols.ul}</li>
+    {/each}
+  </ol>
 </AppShell>
-<style>
-    label, input{
-        padding:5px;
-        margin:5px;
-    }
-    label{
-        font-size: 50px;
-        font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
-    }
-</style>
 
+<style>
+	label,
+	input {
+		padding: 5px;
+		margin: 5px;
+	}
+	label {
+		font-size: 50px;
+		font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+	}
+</style>
